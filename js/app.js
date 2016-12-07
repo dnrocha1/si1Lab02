343bf65cec6671bd267c27000dcc8d3e42ed10dd
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2016-12-07 17:38:11 -0300

    Exibição de tarefa e marcação de concluída

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 var app = angular.module("todoApp", []);
 app.controller("appController", function($scope) {
-	$scope.tarefa = [
-		{}
+	$scope.tasks = [
+		{'title':'Terminar o projeto','done':false},
+		{'title':'Estudar prova de SI1','done':false}
 	];
+
+	$scope.addTask = function(){
+
+	}
+
+	$scope.clearDone = function(){
+
+	}
 });
