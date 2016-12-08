fca3368a21b8d8837d51d70cb8ab6231d07d7814
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2016-12-08 10:17:02 -0300

    Adicionando tarefa na lista

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 var app = angular.module("todoApp", []);
 app.controller("appController", function($scope) {
-	$scope.tasks = [
+	$scope.tasksList = [
 		{'title':'Terminar o projeto','done':false},
 		{'title':'Estudar prova de SI1','done':false},
 		{'title':'Comecar o lab03 de SI1','done':false}
 	];
 
 	$scope.addTask = function(){
-
+		$scope.tasksList.push({title:$scope.tasksInput, done:false});
+		$scope.tasksInput = "";
 	}
 
 	$scope.clearDone = function(){
