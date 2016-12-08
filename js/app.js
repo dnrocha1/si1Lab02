62a4730257c60ab6c9647fe60cf2be3260ad4958
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2016-12-08 00:09:02 -0300

    Commit final do dia

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ var app = angular.module("todoApp", []);
 app.controller("appController", function($scope) {
 	$scope.tasks = [
 		{'title':'Terminar o projeto','done':false},
-		{'title':'Estudar prova de SI1','done':false}
+		{'title':'Estudar prova de SI1','done':false},
+		{'title':'Comecar o lab03 de SI1','done':false}
 	];
 
 	$scope.addTask = function(){
