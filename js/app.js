a53b1c84bb5e9ac80f07fcfc3152dba0315e90ec
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2016-12-08 21:40:22 -0300

    Criação TaskApp, troca de table por lista e implementação de barra de progresso

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 var app = angular.module("todoApp", []);
 app.controller("appController", function($scope) {
+
 	$scope.tasksList = [
-		{'title':'Terminar o projeto','done':false},
-		{'title':'Estudar prova de SI1','done':false},
-		{'title':'Comecar o lab03 de SI1','done':false}
+		{'title':'Terminar o projeto',isDone:false},
+		{'title':'Estudar prova de SI1',isDone:false},
+		{'title':'Comecar o lab03 de SI1',isDone:false}
 	];
 
 	$scope.addTask = function(){
-		$scope.tasksList.push({title:$scope.tasksInput, done:false});
+		$scope.tasksList.push({title:$scope.tasksInput, isDone:false});
 		$scope.tasksInput = "";
 	}
 
 	$scope.clearDone = function(){
 
+	}
+
+	$scope.getNumDone = function(){
+		var count = 0;
+		angular.forEach($scope.tasksList, function(task){
+			if (task.isDone) {
+				count++;
+			}
+		});
+		return count;
+	}
+
+	$scope.getProgressPercentage = function(){
+		return ($scope.getNumDone()/$scope.tasksList.length)*100;
 	}
 });
